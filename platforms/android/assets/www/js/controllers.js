4adb945de128aa6a1e6d970811f987ab0307f3a5
Zeitgeb3r <zeitgeb3r@gmail.com>
2015-05-06 15:08:27 +1000

    Added platform, as the command "ionic platform add android" doesn't work behind proxy firewall.

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers', [])
+
+.controller('AboutCtrl', function($scope) {})
+
+.controller('SnapCtrl', function($scope, Chats) {})
+
+.controller('MyPACCtrl', function($scope){});
