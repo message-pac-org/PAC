46228d4f271429eac8f62ce6523e3e60470a17cb
Zeitgeb3r <zeitgeb3r@gmail.com>
2015-05-07 00:51:08 +1000

    Removed platform directory. It should be regenerated after every git clone and should never be checked in.
    Also added Vertical dots on top nav bar.

--- platforms/android/assets/www/js/controllers.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.controllers', [])
-
-.controller('AboutCtrl', function($scope) {})
-
-.controller('SnapCtrl', function($scope, Chats) {})
-
-.controller('MyPACCtrl', function($scope){});
