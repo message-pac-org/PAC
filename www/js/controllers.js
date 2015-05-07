11b67de77b66986e1399196f944d807e0172f89e
Zeitgeb3r <zeitgeb3r@gmail.com>
2015-05-07 23:32:46 +1000

    Implement PopOver

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,33 +6,43 @@ angular.module('starter.controllers', [])
 
 .controller('MyPACCtrl', function($scope){})
 
-.controller('PopOverCtrl', function($scope, $ionicPopover){
-    $ionicPopover.fromTemplateUrl('templates/tab-popOver.html', { scope: $scope})
-        .then(function(popover) {
-            $scope.popover = popover;
+.controller('PopOverCtrl', function($scope, $ionicPopover) {
+      //var template = '<ion-popover-view><ion-content>Version 0.0.1</ion-content></ion-popover-view>';
+    //   var template = '<ion-popover-view>Version 0.0.1</ion-popover-view>';
+    //   $scope.popover = $ionicPopover.fromTemplate(template, {
+    //     scope: $scope
+    //   });
+
+      $ionicPopover.fromTemplateUrl('tab-popOver.html', {
+        scope: $scope
+      }).then(function(popover) {
+        $scope.popover = popover;
+      });
+
+      $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+      };
+      $scope.closePopover = function() {
+        $scope.popover.hide();
+      };
+
+      $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+      });
+
+      $scope.$on('popover.hidden', function() {
+        // Execute action
+      });
+
+      $scope.$on('popover.removed', function() {
+        // Execute action
+      });
+
+      $scope.demo = 'ios';
+      $scope.setPlatform = function(p) {
+        document.body.classList.remove('platform-ios');
+        document.body.classList.remove('platform-android');
+        document.body.classList.add('platform-' + p);
+        $scope.demo = p;
+      }      
     });
-
-    $scope.openPopover = function($event) {
-      $scope.popover.show($event);
-    };
-
-    $scope.closePopover = function() {
-      $scope.popover.hide();
-    };
-    //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.popover.remove();
-    });
-
-    // Execute action on hide popover
-    $scope.$on('popover.hidden', function() {
-      // Execute action
-    });
-
-    // Execute action on remove popover
-    $scope.$on('popover.removed', function() {
-      // Execute action
-    });
-  })
-
-;
